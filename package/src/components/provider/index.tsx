9e4ac949ae873d642b19dda2163c597a4f8e1df2
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-03-08 18:32:34 +0800

    refactor: pack states into hook

--- package/src/components/provider/index.tsx
+++ package/src/components/provider/index.tsx
@@ -3,10 +3,11 @@
 import type { Format } from "ts-vista";
 
 import type { Options } from "#/@types/options";
+import type { ScrollCoreStates } from "#/contexts/scrollcore";
 
 import * as React from "react";
 
-import { ScrollCoreContext } from "#/contexts/scrollcore";
+import { ScrollCoreContext, useScrollCoreStates } from "#/contexts/scrollcore";
 
 /** Props for the `Provider` component. */
 type ProviderProps = Format<
@@ -19,25 +20,8 @@ type ProviderProps = Format<
 const Provider = (options: ProviderProps): React.JSX.Element => {
     const contentRef = React.useRef<HTMLDivElement | null>(null);
 
-    const [hvTrackX, setHvTrackX] = React.useState<boolean>(false);
-    const [hvThumbX, setHvThumbX] = React.useState<boolean>(false);
-    const [hvTrackY, setHvTrackY] = React.useState<boolean>(false);
-    const [hvThumbY, setHvThumbY] = React.useState<boolean>(false);
-
-    const totalX = React.useRef<number>(0);
-    const totalY = React.useRef<number>(0);
-
-    const viewX = React.useRef<number>(0);
-    const viewY = React.useRef<number>(0);
-
-    const viewOffsetX = React.useRef<number>(0);
-    const viewOffsetY = React.useRef<number>(0);
-
-    const [scrollbarLengthX, setScrollbarLengthX] = React.useState<number>(0);
-    const [scrollbarLengthY, setScrollbarLengthY] = React.useState<number>(0);
-
-    const [scrollbarOffsetX, setScrollbarOffsetX] = React.useState<number>(0);
-    const [scrollbarOffsetY, setScrollbarOffsetY] = React.useState<number>(0);
+    const x: ScrollCoreStates = useScrollCoreStates();
+    const y: ScrollCoreStates = useScrollCoreStates();
 
     return (
         <ScrollCoreContext
@@ -49,32 +33,8 @@ const Provider = (options: ProviderProps): React.JSX.Element => {
                     plugins: options.plugins ?? [],
                 },
                 contentRef,
-                x: {
-                    hvTrack: hvTrackX,
-                    setHvTrack: setHvTrackX,
-                    hvThumb: hvThumbX,
-                    setHvThumb: setHvThumbX,
-                    total: totalX,
-                    view: viewX,
-                    viewOffset: viewOffsetX,
-                    scrollbarLength: scrollbarLengthX,
-                    setScrollbarLength: setScrollbarLengthX,
-                    scrollbarOffset: scrollbarOffsetX,
-                    setScrollbarOffset: setScrollbarOffsetX,
-                },
-                y: {
-                    hvTrack: hvTrackY,
-                    setHvTrack: setHvTrackY,
-                    hvThumb: hvThumbY,
-                    setHvThumb: setHvThumbY,
-                    total: totalY,
-                    view: viewY,
-                    viewOffset: viewOffsetY,
-                    scrollbarLength: scrollbarLengthY,
-                    setScrollbarLength: setScrollbarLengthY,
-                    scrollbarOffset: scrollbarOffsetY,
-                    setScrollbarOffset: setScrollbarOffsetY,
-                },
+                x,
+                y,
             }}
         >
             {options.children}
--- package/src/contexts/scrollcore.ts
+++ package/src/contexts/scrollcore.ts
@@ -22,6 +22,35 @@ type ScrollCoreStates = {
     setScrollbarOffset: React.Dispatch<React.SetStateAction<number>>;
 };
 
+const useScrollCoreStates = (): ScrollCoreStates => {
+    const [hvTrack, setHvTrack] = React.useState<boolean>(false);
+    const [hvThumb, setHvThumb] = React.useState<boolean>(false);
+
+    const total = React.useRef<number>(0);
+
+    const view = React.useRef<number>(0);
+
+    const viewOffset = React.useRef<number>(0);
+
+    const [scrollbarLength, setScrollbarLength] = React.useState<number>(0);
+
+    const [scrollbarOffset, setScrollbarOffset] = React.useState<number>(0);
+
+    return {
+        hvTrack,
+        setHvTrack,
+        hvThumb,
+        setHvThumb,
+        total,
+        view,
+        viewOffset,
+        scrollbarLength,
+        setScrollbarLength,
+        scrollbarOffset,
+        setScrollbarOffset,
+    };
+};
+
 /** Core for internal logic. */
 type ScrollCore = {
     options: ScrollCoreOptions;
@@ -46,4 +75,4 @@ const useScrollCore = (): ScrollCore => {
 };
 
 export type { ScrollCoreOptions, ScrollCoreStates, ScrollCore };
-export { ScrollCoreContext, useScrollCore };
+export { useScrollCoreStates, ScrollCoreContext, useScrollCore };
