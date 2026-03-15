b24f9229d6ce4025a4e7394763dbdbbb3f78cbc1
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-03-15 09:38:22 +0800

    chore: upgrade dependencies

--- package/tsdown.config.ts
+++ package/tsdown.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(
             "hooks/thumb": "./src/hooks/thumb.ts",
         },
         platform: "browser",
+        unbundle: true,
     },
     [
         esmPreset(),
